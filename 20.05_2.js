1a33acde249e91441d21f1d3c73af79d1e5245f1
Ichumikov <Ichumikov@gmail.com>
2025-05-17 22:25:16 +0300

    hw_20.05_2

---
+++ 20.05_2.js
@@ -0,0 +1,62 @@
+// Задание 2.1 БЛОК СХЕМА
+
+// let a = Number(prompt("Введите a"))
+// let b = Number(prompt("Введите b"))
+// let x
+// if (a > b) {
+//     alert(x = a * b)
+// } else if (a < b) {
+//     alert(x = a + b)
+// }
+
+// КАЛЬКУЛЯТОР ВОЗРАСТА
+
+// let yearborn = prompt("Введите год рождения")
+// let age = `Вам ${2025 - yearborn} лет`
+// alert (age)
+
+//ОПРЕДЕЛЕНИЕ МАКСИМАЛЬНОГО ЧИСЛА
+
+// let a = Number(prompt("Введите число а"))
+// let b = Number(prompt("Введите число b"))
+// let c = Number(prompt("Введите число c"))
+
+// if ( a > b && a > c ) {
+//     alert(a)
+// } else if ( b > a && b > c) {
+//     alert(b)
+// } else if ( c > a && c > b) {
+//     alert(c)
+// }
+
+//ПРОВЕРКА ПАРОЛЯ
+
+// let password = prompt("Введите пароль:")
+// let reppass = prompt("Введите пароль еще раз:")
+
+// if(password === reppass) {
+//     alert("Доступ разрешен")
+// } else {
+//     alert("Ошибка")
+// }
+
+// СУММА ЧИСЕЛ
+
+// let a = 1
+// let b = Number(prompt("Введите целое число"))
+// let summ
+// if (b < a) {
+//     alert("Введите целое число больше 1")
+// } else if ( summ = b * ( b + 1) / 2 ) {
+//     alert(summ)
+// }
+
+// ПЕРЕВОД ЦЕЛЬСИЯ В ФАРЕНГЕЙТ И ОБРАТНО
+
+let c = Number(prompt("Введите температуру в цельсиях"))
+let f = Number(prompt("Введите температуру в фаренгейтах"))
+let resc
+let resf
+
+console.log(resc = (f - 32) / 1.8)
+console.log(resf = c * 1.8 + 32)
